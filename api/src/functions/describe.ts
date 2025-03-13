c3fd52745fd3569eb405ff20ae7838611b959b2d
nikekson <imbatman0xff@gmail.com>
2025-03-13 22:03:23 +0200

    api: Cache descriptions in table storage

--- api/src/functions/describe.ts
+++ api/src/functions/describe.ts
@@ -1,6 +1,7 @@
 import createClient, { ImageAnalysisResultOutput } from '@azure-rest/ai-vision-image-analysis';
 import { AzureKeyCredential } from '@azure/core-auth';
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from '@azure/functions';
+import { addDescription, DescriptionEntity, getBlobContentMD5, getDescription } from '../lib/azure-storage';
 
 async function describe(
   request: HttpRequest,
@@ -9,18 +10,16 @@ async function describe(
   try {
     const {
       StorageAccountName,
+      StorageAccountKey,
+      StorageTableName,
       ComputerVisionName,
       ComputerVisionKey
     } = process.env ?? {};
 
-    if (!StorageAccountName || !ComputerVisionName || !ComputerVisionKey) {
+    if (!StorageAccountName || !StorageAccountKey || !StorageTableName || !ComputerVisionName || !ComputerVisionKey) {
       console.error('Missing required app configuration');
       return { status: 500 };
     }
-
-    const credential = new AzureKeyCredential(ComputerVisionKey);
-    const computerVisionEndpoint = `https://${ComputerVisionName}.cognitiveservices.azure.com`;
-    const client = createClient(computerVisionEndpoint, credential);
 
     const containerName = request.query.get('container');
     const filename = request.query.get('file');
@@ -28,6 +27,23 @@ async function describe(
     if (!containerName || !filename) {
       return { status: 400 };
     }
+
+    const md5 = await getBlobContentMD5(StorageAccountName, StorageAccountKey, containerName, filename);
+    const cachedDescription = await getDescription(StorageAccountName, StorageAccountKey, StorageTableName, md5);
+
+    if (cachedDescription) {
+      console.log('Cached description found');
+
+      const { caption, confidence } = cachedDescription;
+
+      return {
+        jsonBody: { caption, confidence }
+      };
+    }
+
+    const credential = new AzureKeyCredential(ComputerVisionKey);
+    const computerVisionEndpoint = `https://${ComputerVisionName}.cognitiveservices.azure.com`;
+    const client = createClient(computerVisionEndpoint, credential);
 
     const features = [
       'Caption',
@@ -41,11 +57,17 @@ async function describe(
       contentType: 'application/json',
     })).body as ImageAnalysisResultOutput;
 
+    const caption = result.captionResult.text;
+    const confidence = result.captionResult.confidence;
+
+    const description: DescriptionEntity = {
+      caption,
+      confidence,
+    };
+    await addDescription(StorageAccountName, StorageAccountKey, StorageTableName, md5, description);
+
     return {
-      jsonBody: {
-        caption: result.captionResult.text,
-        confidence: result.captionResult.confidence,
-      }
+      jsonBody: { caption, confidence }
     };
   } catch (error) {
     return {
--- api/src/lib/azure-storage.ts
+++ api/src/lib/azure-storage.ts
@@ -1,10 +1,19 @@
+import { AzureNamedKeyCredential, RestError, TableClient } from '@azure/data-tables';
 import {
   BlobSASPermissions,
   BlobServiceClient,
+  BlockBlobClient,
   ContainerClient,
   SASProtocol,
   StorageSharedKeyCredential
 } from '@azure/storage-blob';
+
+const TABLE_PARTITION_KEY = 'default';
+
+export type DescriptionEntity = {
+  caption: string;
+  confidence: number;
+};
 
 function getBlobServiceClient(
   serviceName: string,
@@ -23,6 +32,18 @@ function getBlobServiceClient(
   return blobServiceClient;
 }
 
+function getTableServiceClient(
+  serviceName: string,
+  serviceKey: string,
+  tableName: string
+): TableClient {
+  return new TableClient(
+    `https://${serviceName}.table.core.windows.net`,
+    tableName,
+    new AzureNamedKeyCredential(serviceName, serviceKey)
+  );
+}
+
 async function createContainer(
   containerName: string,
   blobServiceClient: BlobServiceClient
@@ -33,24 +54,60 @@ async function createContainer(
   return containerClient;
 }
 
+async function getBlockBlobClient(
+  serviceName: string,
+  serviceKey: string,
+  containerName: string,
+  filename: string
+): Promise<BlockBlobClient> {
+  const blobServiceClient = getBlobServiceClient(serviceName, serviceKey);
+  const containerClient = await createContainer(
+    containerName,
+    blobServiceClient
+  );
+  return containerClient.getBlockBlobClient(filename);
+}
+
+export async function getBlobContentMD5(
+  serviceName: string,
+  serviceKey: string,
+  containerName: string,
+  filename: string
+): Promise<string | null> {
+  const blockBlobClient = await getBlockBlobClient(
+    serviceName,
+    serviceKey,
+    containerName,
+    filename
+  );
+
+  const { contentMD5 } = await blockBlobClient.getProperties();
+
+  if (contentMD5) {
+    return Buffer.from(contentMD5).toString('hex');
+  }
+
+  return null;
+}
+
 export async function generateSASUrl(
   serviceName: string,
   serviceKey: string,
   containerName: string,
   filename: string,
   permissions = 'r',
-  timerangeMinutes = 1,
+  timerangeMinutes = 1
 ): Promise<string> {
   if (!serviceName || !serviceKey || !filename || !containerName) {
     return 'Generate SAS function missing parameters';
   }
 
-  const blobServiceClient = getBlobServiceClient(serviceName, serviceKey);
-  const containerClient = await createContainer(
+  const blockBlobClient = await getBlockBlobClient(
+    serviceName,
+    serviceKey,
     containerName,
-    blobServiceClient
+    filename
   );
-  const blockBlobClient = containerClient.getBlockBlobClient(filename);
 
   const accountSasTokenUrl = await blockBlobClient.generateSasUrl({
     startsOn: new Date(),
@@ -60,4 +117,55 @@ export async function generateSASUrl(
   });
 
   return accountSasTokenUrl;
+}
+
+export async function getDescription(
+  serviceName: string,
+  serviceKey: string,
+  tableName: string,
+  md5: string
+): Promise<DescriptionEntity | null> {
+  const client = getTableServiceClient(serviceName, serviceKey, tableName);
+
+  try {
+    return await client.getEntity<DescriptionEntity>(
+      TABLE_PARTITION_KEY,
+      md5,
+    );
+  } catch (error) {
+    if (error instanceof RestError) {
+      if (error.statusCode === 404) {
+        return null;
+      }
+    }
+
+    throw error;
+  }
+}
+
+export async function addDescription(
+  serviceName: string,
+  serviceKey: string,
+  tableName: string,
+  md5: string,
+  description: DescriptionEntity,
+) {
+  const client = getTableServiceClient(serviceName, serviceKey, tableName);
+
+  try {
+    await client.createEntity<DescriptionEntity>({
+      partitionKey: TABLE_PARTITION_KEY,
+      rowKey: md5,
+      ...description,
+    });
+  } catch (error) {
+    if (error instanceof RestError) {
+      if (error.statusCode === 409) {
+        // Already exists, ignore
+        return;
+      }
+    }
+
+    throw error;
+  }
 }
